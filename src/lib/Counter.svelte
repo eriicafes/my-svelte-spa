b2e82b32e611ec4cfb03d09306be508b9436a42a
Eric Afes <eriicafes@gmail.com>
2022-11-03 20:08:24 +0100

    Start of my svelte journey

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+  const log = (node, opt, etc) => {
+    console.log({opt, etc})
+    console.log(node)
+  }
+</script>
+
+<button on:click={increment} use:log>
+  count is {count}
+</button>
